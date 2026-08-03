9eec8b4f39a0f87df366ec3d93b9988f431c6755
PhamNguyen <jim.phamnguyen@gmail.com>
2026-08-03 13:04:21 +0700

    fix: logic Controller Admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,22 +2,23 @@
 class Admin extends Controller {
     
     public function __construct() {
-        // 1. Kiểm tra xem user đã đăng nhập chưa
-        if ($_SESSION['user_role'] != 3) {
-            header('location: ' . URLROOT);
-            exit();
-        }
-        
-        // 2. Kiểm tra xem user có phải là Admin (role_id = 3) không
-        if ($_SESSION['user_role'] = 3) {
-            // Nếu không phải Admin, đá văng về trang chủ
-            header('location: ' . URLROOT);
-            exit();
-        }
-        
-        // Nếu qua được 2 ải trên, có thể nạp các Model cần thiết cho Admin
-        // $this->userModel = $this->model('User');
+    // 1. Kiểm tra xem user đã đăng nhập chưa bằng cách check user_id
+    if (!isset($_SESSION['user_id'])) {
+        // Chưa đăng nhập thì đá về trang Login
+        header('location: ' . URLROOT . '/users/login');
+        exit();
     }
+    
+    // 2. Kiểm tra xem user có phải là Admin (role = 3) không
+    if ($_SESSION['user_role'] != 3) {
+        // Nếu đã đăng nhập nhưng KHÔNG phải Admin, đá văng về trang chủ
+        header('location: ' . URLROOT);
+        exit();
+    }
+    
+    // Nếu lọt qua được 2 ải trên, chính thức được công nhận là Admin!
+    // $this->userModel = $this->model('User');
+}
 
     // Chức năng UC39: Dashboard Admin
     public function dashboard() {
